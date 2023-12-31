1140bee1eeac7d7e4e0dc3f333212fb9246b300e
Nicolas <nicolas.rodi@ross.com.ar>
2023-12-31 02:22:19 -0300

    wip applying grid col span breakpoints within the todo card component

--- app/components/card_view/TodoCard.tsx
+++ app/components/card_view/TodoCard.tsx
@@ -121,149 +121,152 @@ export default function TodoCard({ todo }: TodoCardProps) {
   }
 
   return (
-    <Card
-      className={`group relative ${
-        todo.done
-          ? 'rounded-md border border-transparent'
-          : 'border border-transparent'
-      }`}
+    <div
+      className={`group relative sm:col-span-1
+    md:col-span-2 lg:col-span-1 ${
+      todo.done
+        ? 'rounded-md border border-emerald-500'
+        : 'rounded-md border border-zinc-600'
+    }`}
     >
-      <div className='mb-2 ml-5  justify-items-start'>
-        <form className='items-center '>
-          {isEditing ? (
-            <>
-              <CardHeader>
-                <CardTitle>
+      <Card className='border border-transparent'>
+        <div className='mb-2 ml-5  justify-items-start'>
+          <form className='items-center '>
+            {isEditing ? (
+              <>
+                <CardHeader>
+                  <CardTitle>
+                    <input
+                      type='text'
+                      className='mr-2 mt-2 rounded border border-gray-300 px-2 py-1 text-lg'
+                      value={editedName}
+                      onChange={e => {
+                        setEditedName(e.target.value)
+                        setNameEdited(true)
+                      }}
+                    />
+                  </CardTitle>
+                </CardHeader>
+                <CardContent>
                   <input
                     type='text'
                     className='mr-2 mt-2 rounded border border-gray-300 px-2 py-1 text-lg'
-                    value={editedName}
+                    value={editedMessage}
                     onChange={e => {
-                      setEditedName(e.target.value)
-                      setNameEdited(true)
+                      setEditedMessage(e.target.value)
+                      setMessageEdited(true)
                     }}
                   />
-                </CardTitle>
-              </CardHeader>
-              <CardContent>
-                <input
-                  type='text'
-                  className='mr-2 mt-2 rounded border border-gray-300 px-2 py-1 text-lg'
-                  value={editedMessage}
-                  onChange={e => {
-                    setEditedMessage(e.target.value)
-                    setMessageEdited(true)
-                  }}
-                />
 
-                <div className='mt-2'>
-                  <DatePickerDemo
-                    onSelectDate={() => {
-                      setDateEdited(true)
-                    }}
-                    selectedDate={editedDate}
-                    onDateChange={setEditedDate}
-                  />
-                </div>
-              </CardContent>
-              <SubmitButton
-                style='mr-2 bg-indigo-500 p-1 border border-0 rounded-md hover:bg-zinc-400 dark:hover:bg-slate-700 mt-1'
-                formAction={() => {
-                  switchHandleConfirmClick()
-                }}
-              >
-                <p className='rounded-md text-sm text-zinc-200'>Confirm</p>
-              </SubmitButton>
-              <button
-                onClick={handleCancelClick}
-                className='mt-1 rounded-md border-0 bg-rose-800 p-1 hover:bg-zinc-400 dark:hover:bg-slate-700'
-              >
-                <p className='rounded-md text-sm text-zinc-200'>Cancel</p>
-              </button>
-            </>
-          ) : (
-            <>
-              <CardHeader>
-                <CardTitle>{todo.name}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p>
-                  {todo.message.length <= 40
-                    ? todo.message
-                    : `${todo.message.slice(0, 40)}...`}
-                </p>
-
-                <div className='mb-2 mt-2 flex flex-row gap-2'>
-                  <Badge variant='secondary' className='border-zinc-600 p-2'>
-                    <p className='text-xs'>Hecho: </p>
-                  </Badge>
-
-                  <Checkbox
-                    checked={todo.done}
-                    disabled={isCheckingDone}
-                    onCheckedChange={handleCheckedChange}
-                    className='border-1 ml-2 mr-2 mt-2 h-5 w-5 rounded-sm border border-zinc-500'
-                    id='done'
-                  />
-
-                  {isCheckingDone && (
-                    <TailSpin
-                      height='20'
-                      width='20'
-                      color='#4fa94d'
-                      ariaLabel='tail-spin-loading'
-                      radius='1'
-                      visible={true}
-                      wrapperClass='bg-inherit'
+                  <div className='mt-2'>
+                    <DatePickerDemo
+                      onSelectDate={() => {
+                        setDateEdited(true)
+                      }}
+                      selectedDate={editedDate}
+                      onDateChange={setEditedDate}
                     />
-                  )}
-                </div>
-
-                <div className='mb-2 mt-2 flex flex-row gap-2'>
-                  {todo.scheduledDate ? (
-                    <>
-                      <Badge
-                        variant='secondary'
-                        className='border-zinc-600 p-2'
-                      >
-                        <p className='text-xs'>Fechado: </p>
-                      </Badge>
-                      <p className='mt-2 text-sm'>
-                        {todo.scheduledDate.toDateString()}
-                      </p>
-                    </>
-                  ) : (
-                    <>
-                      <Badge variant='secondary' className='hidden'>
-                        <p className='hidden'>Fechado: </p>
-                      </Badge>
-                      <p className='hidden'>''</p>
-                    </>
-                  )}
-                </div>
-              </CardContent>
-            </>
-          )}
-
-          <CardFooter>
-            <div className='absolute right-0 mt-5'>
-              <div className='duration-50 -translate-y-1/2 transform opacity-0 transition group-hover:opacity-100'>
-                <button type='button' onClick={handleEditClick}>
-                  <PencilIcon className='h-5 w-5 text-blue-600' />
-                </button>
-
+                  </div>
+                </CardContent>
                 <SubmitButton
-                  formAction={async () => {
-                    await deleteTodoItem(todo._id)
+                  style='mr-2 bg-indigo-500 p-1 border border-0 rounded-md hover:bg-zinc-400 dark:hover:bg-slate-700 mt-1'
+                  formAction={() => {
+                    switchHandleConfirmClick()
                   }}
                 >
-                  <TrashIcon className='h-5 w-5 text-red-600' />
+                  <p className='rounded-md text-sm text-zinc-200'>Confirm</p>
                 </SubmitButton>
+                <button
+                  onClick={handleCancelClick}
+                  className='mt-1 rounded-md border-0 bg-rose-800 p-1 hover:bg-zinc-400 dark:hover:bg-slate-700'
+                >
+                  <p className='rounded-md text-sm text-zinc-200'>Cancel</p>
+                </button>
+              </>
+            ) : (
+              <>
+                <CardHeader>
+                  <CardTitle>{todo.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p>
+                    {todo.message.length <= 40
+                      ? todo.message
+                      : `${todo.message.slice(0, 40)}...`}
+                  </p>
+
+                  <div className='mb-2 mt-2 flex flex-row gap-2'>
+                    <Badge variant='secondary' className='border-zinc-600 p-2'>
+                      <p className='text-xs'>Hecho: </p>
+                    </Badge>
+
+                    <Checkbox
+                      checked={todo.done}
+                      disabled={isCheckingDone}
+                      onCheckedChange={handleCheckedChange}
+                      className='border-1 ml-2 mr-2 mt-2 h-5 w-5 rounded-sm border border-zinc-500'
+                      id='done'
+                    />
+
+                    {isCheckingDone && (
+                      <TailSpin
+                        height='20'
+                        width='20'
+                        color='#4fa94d'
+                        ariaLabel='tail-spin-loading'
+                        radius='1'
+                        visible={true}
+                        wrapperClass='bg-inherit'
+                      />
+                    )}
+                  </div>
+
+                  <div className='mb-2 mt-2 flex flex-row gap-2'>
+                    {todo.scheduledDate ? (
+                      <>
+                        <Badge
+                          variant='secondary'
+                          className='border-zinc-600 p-2'
+                        >
+                          <p className='text-xs'>Fechado: </p>
+                        </Badge>
+                        <p className='mt-2 text-sm'>
+                          {todo.scheduledDate.toDateString()}
+                        </p>
+                      </>
+                    ) : (
+                      <>
+                        <Badge variant='secondary' className='hidden'>
+                          <p className='hidden'>Fechado: </p>
+                        </Badge>
+                        <p className='hidden'>''</p>
+                      </>
+                    )}
+                  </div>
+                </CardContent>
+              </>
+            )}
+
+            <CardFooter>
+              <div className='absolute right-0 mt-5'>
+                <div className='duration-50 -translate-y-1/2 transform opacity-0 transition group-hover:opacity-100'>
+                  <button type='button' onClick={handleEditClick}>
+                    <PencilIcon className='h-5 w-5 text-blue-600' />
+                  </button>
+
+                  <SubmitButton
+                    formAction={async () => {
+                      await deleteTodoItem(todo._id)
+                    }}
+                  >
+                    <TrashIcon className='h-5 w-5 text-red-600' />
+                  </SubmitButton>
+                </div>
               </div>
-            </div>
-          </CardFooter>
-        </form>
-      </div>
-    </Card>
+            </CardFooter>
+          </form>
+        </div>
+      </Card>
+    </div>
   )
 }
